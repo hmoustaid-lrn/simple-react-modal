7a493646759acefe22e9634b078bd8d28fdb8793
Hicham Moustaid <hmoustaid.lrn@gmail.com>
2023-09-08 17:26:39 +0200

    Fix some errors and Republish

--- dist/components/Modal/index.js
+++ dist/components/Modal/index.js
@@ -4,29 +4,31 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
+var _react = _interopRequireDefault(require("react"));
 require("bootstrap/dist/css/bootstrap.min.css");
 require("./index.css");
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 const Modal = props => {
   const handleToggle = () => {
     props.toggleModal(!props.showingModal);
   };
-  return /*#__PURE__*/React.createElement(React.Fragment, null, props.showingModal && /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("div", {
+  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, props.showingModal && /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("div", {
     className: "modal show",
     tabIndex: "-1",
     role: "dialog",
     style: {
       display: 'block'
     }
-  }, /*#__PURE__*/React.createElement("div", {
+  }, /*#__PURE__*/_react.default.createElement("div", {
     className: "modal-dialog",
     role: "document"
-  }, /*#__PURE__*/React.createElement("div", {
+  }, /*#__PURE__*/_react.default.createElement("div", {
     className: "modal-content"
-  }, /*#__PURE__*/React.createElement("div", {
+  }, /*#__PURE__*/_react.default.createElement("div", {
     className: "modal-body text-center"
-  }, /*#__PURE__*/React.createElement("h4", null, props.text)), /*#__PURE__*/React.createElement("div", {
+  }, /*#__PURE__*/_react.default.createElement("h4", null, props.text)), /*#__PURE__*/_react.default.createElement("div", {
     className: "modal-footer d-flex justify-content-center"
-  }, /*#__PURE__*/React.createElement("button", {
+  }, /*#__PURE__*/_react.default.createElement("button", {
     type: "button",
     className: "btn btn-primary",
     onClick: handleToggle
